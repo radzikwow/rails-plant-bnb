dec29c7ba09f9682facf3be660f2b9d65e813057
benedetta castagna <benny.castagna@gmail.com>
2022-06-01 15:09:57 +0200

    javascript added to home page

---
+++ app/javascript/controllers/home_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["card"]
+  connect() {
+    console.log("Hello from our first Stimulus controller")
+  }
+
+  hideCards(event){
+    console.log(event)
+    this.cardTarget.classList.remove("d-none")
+  }
+}
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,9 @@
 
 import { application } from "./application"
 
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import HomeController from "./home_controller"
+application.register("home", HomeController)
