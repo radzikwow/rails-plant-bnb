46a1e2e0d992717cfb2f5b55e6f5f2600322cd13
benedetta castagna <benny.castagna@gmail.com>
2022-06-01 13:48:58 +0200

    show page without map

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Offer.destroy_all
+ Offer.destroy_all
 
 user = User.create(email: "lalala@lala.com", password: "lalala")
 
-Offer.create!(price: 15, area: "London", user: user)
-Offer.create!(price: 10, area: "Manchester", user: user)
-Offer.create!(price: 25, area: "Madrid", user: user)
-Offer.create!(price: 11, area: "Lisbon", user: user)
-Offer.create!(price: 2, area: "Warsaw", user: user)
-Offer.create!(price: 14, area: "Istanbul", user: user)
-Offer.create!(price: 1, area: "Taiwan", user: user)
+Offer.create!(price: 15, area: "London", user: user, description: 'a very nice description for the plant-keeping')
+Offer.create!(price: 10, area: "Manchester", user: user, description: 'a very nice description for the plant-keeping')
+Offer.create!(price: 25, area: "Madrid", user: user, description: 'a very nice description for the plant-keeping')
+Offer.create!(price: 11, area: "Lisbon", user: user, description: 'a very nice description for the plant-keeping')
+Offer.create!(price: 2, area: "Warsaw", user: user, description: 'a very nice description for the plant-keeping')
+Offer.create!(price: 14, area: "Istanbul", user: user, description: 'a very nice description for the plant-keeping')
+Offer.create!(price: 1, area: "Taiwan", user: user, description: 'a very nice description for the plant-keeping')
