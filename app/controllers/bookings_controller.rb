3a8a64f5edfd03c4713b2b7c699bfa9f10a2653f
benedetta castagna <benny.castagna@gmail.com>
2022-06-03 11:25:04 +0200

    added bookings page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,13 @@
 class BookingsController < ApplicationController
 
+  # index bookings for a user
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
   def show
     @offer = Offer.find(params[:offer_id])
-    #@booking = Booking.find_by(offer: @offer, user: current_user)
+    # @booking = Booking.find_by(offer: @offer, user: current_user)
     @booking = Booking.find(params[:id])
   end
 
@@ -12,11 +17,13 @@ class BookingsController < ApplicationController
   end
 
   def create
+    raise
     @offer = Offer.find(params[:offer_id])
-    @booking = Booking.new(bookings_params)
+    @booking = Booking.new
+    @booking.start_date = params[:search]["select_dates"][0..9]
+    @booking.end_date = params[:search]["select_dates"][14..23]
     @booking.user = current_user
     @booking.offer = @offer
-
 
     if @booking.save!
       redirect_to offer_booking_path(params[:offer_id], @booking.id)
@@ -29,14 +36,14 @@ class BookingsController < ApplicationController
     @offer = Offer.find(params[:offer_id])
     @booking = Booking.find_by(offer: @offer, user: current_user)
     @booking.destroy
-    redirect_to root_path
+    redirect_to bookings_path
   end
 
-  private
+  # private
 
-  def bookings_params
-    params.require(:booking).permit(:start_date, :end_date)
-  end
+  # def bookings_params
+  #   params.require(:booking).permit(:start_date, :end_date)
+  # end
 end
 
 # offer: @offer, user: current_user
